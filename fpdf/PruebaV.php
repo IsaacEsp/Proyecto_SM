1327c02dc564a6c7f824d4d6db33e541a4d2e473
IsaacEsp <114197628+IsaacEsp@users.noreply.github.com>
2023-04-12 01:06:02 -0400

    Add files via upload

---
+++ fpdf/PruebaV.php
@@ -0,0 +1,145 @@
+<?php
+
+require('./fpdf.php');
+
+class PDF extends FPDF {
+
+    // Cabecera de página
+    function Header() {
+
+        include("../DATABASE/db.php");
+
+        $queryc = "SELECT * FROM paciente
+        CROSS JOIN factura 
+        where paciente.cedula = factura.id_paciente order by id_factura DESC limit 1;";
+        $resultc = mysqli_query($conn, $queryc);
+        $dato = $resultc->fetch_object();
+
+                $querycc = "SELECT * FROM paciente
+        CROSS JOIN paciente_vs_correo ON paciente.cedula = paciente_vs_correo.id_paciente
+        CROSS JOIN factura
+        CROSS JOIN correo
+        where paciente.cedula = factura.id_paciente AND correo.id_correo = paciente_vs_correo.id_correo  order by id_factura DESC limit 1;";
+        $resultcc = mysqli_query($conn, $querycc);
+        $datoc = $resultcc->fetch_object();
+        
+//        $querycc = "SELECT nombre FROM paciente WHERE cedula=$datoc.id_paciente";
+//        $resultcc = mysqli_query($conn, $querycc);
+//        $dato = $resultc->fetch_object();
+
+        $this->Image('../img/Logo.png', 180, 12, 15, 15, 'PNG'); //logo de la empresa,moverDerecha,moverAbajo,tamañoIMG
+        $this->SetFont('Arial', 'B', 19); //tipo fuente, negrita(B-I-U-BIU), tamañoTexto
+        $this->SetTextColor(0, 0, 0); //color
+        //creamos una celda o fila
+        $this->SetFont('Arial', 'B', 20);
+        $this->SetTextColor(32, 100, 210);
+        $this->Cell(150, 20, utf8_decode('Consultorio Medico Dr. Pedro Angel Guzman'), 0, 0, 'C'); // AnchoCelda,AltoCelda,titulo,borde(1-0),saltoLinea(1-0),posicion(L-C-R),ColorFondo(1-0)
+        $this->Ln(20); // Salto de línea
+        /* UBICACION */
+        $this->SetFont('Arial', 'B', 10);
+        $this->SetTextColor(39, 39, 51);
+        $this->Cell(96, 10, utf8_decode("Ubicación : Santiago De Los Caballeros, Santiago, Dominican Republic. "), 0, 0, 'L', 0);
+        $this->Ln(5);
+        /* TELEFONO */
+        $this->SetFont('Arial', 'B', 10);
+        $this->SetTextColor(39, 39, 51);
+        $this->Cell(59, 10, utf8_decode("Teléfono : 809-247-5315"), 0, 0, 'L', 0);
+        $this->Ln(5);
+        /* COREEO */
+        $this->SetFont('Arial', 'B', 10);
+        $this->SetTextColor(39, 39, 51);
+        $this->Cell(85, 10, utf8_decode("Correo :  drpedroangel@outlook.com"), 0, 0, 'L', 0);
+        $this->Ln(5);
+        /* TELEFONO */
+        $this->SetFont('Arial', 'B', 10);
+        $this->SetTextColor(39, 39, 51);
+        $this->Cell(85, 10, utf8_decode("RNC: 1234563"), 0, 0, 'L', 0);
+        $this->Ln(10);
+        //**********************************************DATOS PACIENTE*******************************************************
+
+        $this->SetFont('Arial', 'B', 10);
+        $this->SetTextColor(39, 39, 51);
+        $this->Cell(20, 10, utf8_decode("Paciente : ". $dato->nombre), 0, 0, 'C', 0);
+        $this->Ln(5);
+
+        /* TELEFONO */
+//        $this->SetFont('Arial', '', 10);
+//        $this->SetTextColor(39, 39, 51);
+//        $this->Cell(200, 3, utf8_decode("Teléfono : ".$dato->telefono), 0, 0, 'C', 0);
+
+        $this->Ln(5);
+        /* COREEO */
+        $this->SetFont('Arial', '', 10);
+        $this->SetTextColor(39, 39, 51);
+        $this->Cell(20, 10, utf8_decode("Correo : ".$datoc->correo), 0, 0, 'C', 0);
+        $this->Ln(5);
+        /* CEUDLA */
+        $this->SetFont('Arial', 'B', 10);
+        $this->SetTextColor(39, 39, 51);
+        $this->Cell(182, 0, utf8_decode("Cedula: ".$dato->cedula), 0, 0, 'C', 0);
+        $this->Ln(10);
+        /* TITULO DE LA TABLA */
+        //color
+        $this->SetTextColor(228, 100, 0);
+        $this->Cell(50); // mover a la derecha
+        $this->SetFont('Arial', 'B', 20);
+        $this->Cell(100, 10, utf8_decode("Factura Cliente"), 0, 1, 'C', 0);
+        $this->Ln(7);
+
+        /* CAMPOS DE LA TABLA */
+        //color
+        $this->SetFillColor(228, 100, 0); //colorFondo
+        $this->SetTextColor(255, 255, 255); //colorTexto
+        $this->SetDrawColor(163, 163, 163); //colorBorde
+        $this->SetFont('Arial', 'B', 11);
+        $this->Cell(18, 10, utf8_decode('N°'), 1, 0, 'C', 1);
+        $this->Cell(40, 10, utf8_decode('PROCEDIMIENTO'), 1, 0, 'C', 1);
+        $this->Cell(30, 10, utf8_decode('COSTO'), 1, 0, 'C', 1);
+        $this->Cell(40, 10, utf8_decode('COBERTURA'), 1, 0, 'C', 1);
+        $this->Cell(40, 10, utf8_decode('TOTAL'), 1, 1, 'C', 1);
+      // $this->Cell(25, 10, utf8_decode('ESTADO'), 1, 1, 'C', 1);
+    }
+
+}
+
+// Pie de página
+function Footer() {
+    $this->SetY(-15); // Posición: a 1,5 cm del final
+    $this->SetFont('Arial', 'I', 8); //tipo fuente, negrita(B-I-U-BIU), tamañoTexto
+    $this->Cell(0, 10, utf8_decode('Página ') . $this->PageNo() . '/{nb}', 0, 0, 'C'); //pie de pagina(numero de pagina)
+
+    $this->SetY(-15); // Posición: a 1,5 cm del final
+    $this->SetFont('Arial', 'I', 8); //tipo fuente, cursiva, tamañoTexto
+    $hoy = date('d/m/Y');
+    $this->Cell(355, 10, utf8_decode($hoy), 0, 0, 'C'); // pie de pagina(fecha de pagina)
+}
+
+require("../DATABASE/db.php");
+//include '../../recursos/Recurso_conexion_bd.php';
+//require '../../funciones/CortarCadena.php';
+/* CONSULTA INFORMACION DEL HOSPEDAJE */
+//$consulta_info = $conexion->query(" select *from hotel ");
+//$dato_info = $consulta_info->fetch_object();
+
+$pdf = new PDF();
+$pdf->AddPage(); /* aqui entran dos para parametros (horientazion,tamaño)V->portrait H->landscape tamaño (A3.A4.A5.letter.legal) */
+$pdf->AliasNbPages(); //muestra la pagina / y total de paginas
+
+$i = 0;
+$pdf->SetFont('Arial', '', 12);
+$pdf->SetDrawColor(163, 163, 163); //colorBorde
+
+$query = "SELECT * FROM detalle_factura order by id_factura DESC limit 1";
+$result = mysqli_query($conn, $query);
+/* $consulta_reporte_alquiler = $conexion->query("  "); */
+
+while ($row = $result->fetch_assoc()) {
+    $pdf->Cell(18, 10, $row['id_factura'], 1, 0, 'C', 0);
+    $pdf->Cell(40, 10, $row['id_proc'], 1, 0, 'C', 0);
+    $pdf->Cell(30, 10, $row['precio'], 1, 0, 'C', 0);
+    $pdf->Cell(40, 10, $row['cobertura'], 1, 0, 'C', 0);
+    $pdf->Cell(40, 10, $row['cantidad'], 1, 1, 'C', 0);
+    // $pdf->Cell(25, 10, $row('ESTADO'), 1, 1, 'C', 1);
+}
+
+$pdf->Output('Prueba.pdf', 'I'); //nombreDescarga, Visor(I->visualizar - D->descargar)
---
+++ includes/header.php
@@ -0,0 +1,170 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="../css/alertify.css">
+    <link rel="stylesheet" href="../css/themes/default.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery.dataTables.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/buttons.dataTables.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+  
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Datatables -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/v/bs4-4.1.1/dt-1.10.18/datatables.min.css">
+    <script src="https://cdn.datatables.net/v/bs4-4.1.1/dt-1.10.18/datatables.min.js"></script>
+
+    <!--font awesome con CDN-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+    <!-- CSS --> 
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css"> 
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.2/css/buttons.dataTables.min.css"> 
+
+
+
+    <!-- Buttons -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.min.css">
+    <script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.53/build/pdfmake.min.js"></script>
+    <script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.53/build/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.flash.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+
+    <style>
+        #example tfoot input {
+            width: 100% !important;
+        }
+
+        #example tfoot {
+            display: table-header-group !important;
+        }
+           /* Style for Extra Large Screen */
+@media (max-width:3920px) {
+.btn-sq-responsive {
+width: 200px !important;
+max-width: 100% !important;
+max-height: 100% !important;
+height: 200px !important;
+font-size:24px;
+}
+}
+
+/* Style for Large Screen */
+@media (max-width:991px) {
+.btn-sq-responsive {
+width: 150px !important;
+max-width: 100% !important;
+max-height: 100% !important;
+height: 150px !important;
+font-size:18px;
+}
+}
+
+/* Style for Medium Screen */
+@media (max-width:767px) {
+.btn-sq-responsive {
+width: 100px !important; /* whatever width you want for medium screen */
+max-width: 100% !important;
+max-height: 100% !important;
+height: 100px !important; /* whatever height you want for medium screen */
+font-size:12px;
+}
+}
+
+/* Style for Small Screen */
+@media (max-width:575px) {
+.btn-sq-responsive {
+width: 50px !important; /* whatever width you want for mobile screen */
+max-width: 100% !important;
+max-height: 100% !important;
+height: 50px !important; /* whatever height you want for mobile screen */
+font-size:5px;
+padding: 0px;
+font-size:7px;
+}
+}
+    </style>
+
+
+
+    <title>Sistema Medico</title>
+</head>
+
+<body class="p-0 m-2 border-0 bd-example">
+
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../Pantallas/hola.html">
+                <img src="..\img\Logo.png" alt="Logo" width="30" height="24">
+                Consultorio Medico Dr. Pedro Angel Guzman
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Procesos
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="../Pantallas/Facturacion.php">Facturacion</a></li>
+                            <li><a class="dropdown-item" href="../Pantallas/procesoCobertura.php">Cobertura</a></li>
+
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Mantenimientos
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="../Pantallas/MantenimientoPacientes.php">Mantenimiento pacientes</a></li>
+                            <li><a class="dropdown-item" href="../Pantallas/MantenimientoDoctores.php">Mantenimiento doctores</a></li>
+                            <li><a class="dropdown-item" href="../Pantallas/MantenimientoProcedimientos.php">Mantenimiento procedimientos</a></li>
+                            <li><a class="dropdown-item" href="../Pantallas/MantenimientoAseguradora.php">Mantenimiento aseguradora</a></li>
+                            <li><a class="dropdown-item" href="../Pantallas/MantenimientoUsuario.php">Mantenimiento usuario</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Consultas
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="../Pantallas/ConsultaPaciente.php">Consulta de pacientes</a></li>
+                            <li><a class="dropdown-item" href="../Pantallas/ConsultaDoctores.php">Consulta de doctores</a></li>
+                            <li><a class="dropdown-item" href="../Pantallas/ConsultaProcedimientos.php">Consulta de procedimientos</a></li>
+                            <li><a class="dropdown-item" href="../Pantallas/ConsultaAseguradora.php">Consulta de aseguradora</a></li>
+                            <li><a class="dropdown-item" href="../Pantallas/ConsultaUsuario.php">Consulta de usuarios</a></li>
+                            <li><a class="dropdown-item" href="../Pantallas/ConsultaCobertura.php">Consulta de coberturas</a></li>
+                            <li><a class="dropdown-item" href="../Pantallas/ConsultaFactura.php">Consulta de facturas</a></li>
+
+                        </ul>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+
+
+                </form>
+            </div>
+
+            
+            <a class="btn btn-outline-success" href="../DATABASE/cerrarSesion.php">Cerrar Sesión</a>
+
+          
+        </div>
+    </nav>
